94f56ddc938f7b0df49da2108cf315ea91a082cb
Silvia-Du <silviadu@hotmail.it>
2022-07-04 15:02:50 +0200

    Impostata base verifica dati in edit

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -1,9 +1,25 @@
 @extends('layout.main')
 
 @section('content')
-    <div class="container-fluid px-5 edit">
+    <div class="container-fluid p-5 edit">
+        @if ($errors->any())
+
+        <div class="alert-danger row p-3 rounded-3 mb-3">
+            <div class="col">
+                <ul class="mb-0">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+        @endif
+
+
         <div class="row px-3">
+
             <h1 class="mb-5">Modifica di : {{ $comic->title }}</h1>
+
             <div class="col-4 debug d-flex justify-content-center align-items-center py-4">
                 <img src="{{ $comic->image }}" alt="{{ $comic->title }}">
             </div>
@@ -15,17 +31,35 @@
                     {{-- titolo --}}
                     <div class="mb-3">
                       <label for="title" class="form-label">Title</label>
-                      <input type="text" class="form-control" name="title" id="title" value="{{ $comic->title }}" required>
+                      <input type="text"
+                      class="form-control @error('title') is-invalid @enderror"
+                      name="title" id="title"
+                      value="{{ old('title', $comic->title) }}" required>
+                      @error('title')
+                          <p>{{ $message }}</p>
+                      @enderror
                     </div>
                     {{-- immagine --}}
                     <div class="mb-3">
                       <label for="image" class="form-label">Image</label>
-                      <input type="text" class="form-control" name="image" id="image" value="{{ $comic->image }}" required>
+                      <input type="text"
+                      class="form-control @error('image') is-invalid @enderror"
+                      name="image" id="image"
+                      value="{{ old('image', $comic->image) }}" required>
+                      @error('title')
+                          <p>{{ $message }}</p>
+                      @enderror
                     </div>
                     {{-- type --}}
                     <div class="mb-3">
                       <label for="type" class="form-label">Type</label>
-                      <input type="text" class="form-control" name="type" id="type" value="{{ $comic->type }}" required>
+                      <input type="text"
+                      class="form-control @error('type') is-invalid @enderror"
+                      name="type" id="type"
+                      value="{{ old('type', $comic->type) }}" required>
+                      @error('title')
+                          <p>{{ $message }}</p>
+                      @enderror
                     </div>
 
                     <button type="submit" class="btn btn-primary">Send data</button>
